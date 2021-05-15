dee0215d8d10fb0a9162c05e1943cb350d3f42cc
Jonathan Silverman <jsilverman2@gmail.com>
2021-05-15 00:52:58 -0600

    canvas action... gather data from redis

--- src/controllers/livepixel_controller.cr
+++ src/controllers/livepixel_controller.cr
@@ -1,9 +1,47 @@
 require "redis"
 
 class LivepixelController < ApplicationController
-
-    def canvas
-        random_number = Random.rand(10000).to_i
-        render "canvas.ecr", layout: "livepixel.ecr"
+  def canvas
+    random_number = Random.rand(10000).to_i
+    room = params[:room] rescue ""
+    redis = Redis.new
+    if room == ""
+      ttl = redis.ttl("packets")
+    else
+      ttl = redis.ttl("packets_#{room}")
     end
+    counter = redis.incr("counter")
+
+    ad = ""
+    while ad == ""
+      order_id = redis.srandmember("banner_order_ids").to_s
+      break if order_id == ""
+      ad = redis.get(order_id)
+      banner_link = redis.get("#{order_id}_link")
+      if ad == ""
+        redis.srem("banner_order_ids", order_id)
+      else
+        break
+      end
+    end
+
+    chats = ""
+    if room == ""
+      amt_packets = redis.llen("chats")
+      if amt_packets >= 100
+        chats = redis.lrange("chats", -100, -1)
+      else
+        chats = redis.lrange("chats", 0, -1)
+      end
+    else
+      amt_packets = redis.llen("chats_#{params[:room]}")
+      if amt_packets >= 100
+        chats = redis.lrange("chats_#{params[:room]}", -100, -1)
+      else
+        chats = redis.lrange("chats_#{params[:room]}", 0, -1)
+      end
+    end
+
+    render "canvas.ecr", layout: "livepixel.ecr"
+  end
 end
