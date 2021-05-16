ffb460d6866bc61353f83b5d36a873392b9862b3
Jonathan Silverman <jsilverman2@gmail.com>
2021-05-16 01:53:56 -0600

    ping fix and maybe cam

--- src/channels/session_channel.cr
+++ src/channels/session_channel.cr
@@ -3,7 +3,7 @@ class SessionChannel < Amber::WebSockets::Channel
   end
 
   def handle_message(client_socket, message)
-    SessionSocket.broadcast("message", message.as_h["topic"].to_s, "message_new", message.as_h["payload"].as_h.to_h)
+    rebroadcast!(message)
   end
 
   def handle_leave(client_socket)
