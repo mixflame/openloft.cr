6ea1be96aa2b6ff2f1c51e4c33cf6cf70d6a8a3d
Jonathan Silverman <jsilverman2@gmail.com>
2021-05-17 22:11:30 -0600

    use ice restart

--- src/assets/javascripts/livepixel.js
+++ src/assets/javascripts/livepixel.js
@@ -499,13 +499,14 @@ function setMediaBitrates(sdp) {
     };
   
     pc.oniceconnectionstatechange = () => {
-      if (pc.iceConnectionState == "disconnected") {
+      if (pc.iceConnectionState == "disconnected" || pc.iceConnectionState == "failed") {
         if(!window.dontLog) console.log("Disconnected:", userId);
-        broadcastData({
-          type: REMOVE_USER,
-          from: userId,
-          name: name
-        }); 
+        // broadcastData({
+        //   type: REMOVE_USER,
+        //   from: userId,
+        //   name: name
+        // }); 
+        pc.restartIce();
       }
     };
   
