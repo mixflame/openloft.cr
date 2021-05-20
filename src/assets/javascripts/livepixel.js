da7b49a4aaf973d0f1e36feb59ff03966684c925
Jonathan Silverman <jsilverman2@gmail.com>
2021-05-20 00:03:24 -0600

    turn down doing setRemoteDescription when already stable

--- src/assets/javascripts/livepixel.js
+++ src/assets/javascripts/livepixel.js
@@ -581,7 +581,7 @@ function setMediaBitrates(sdp) {
       const offerCollision = (data.sdp.type == "offer") &&
                                (window.makingOffer || pc.signalingState != "stable");
       ignoreOffer = !polite && offerCollision;
-      if (ignoreOffer) {
+      if (ignoreOffer || pc.signalingState == "stable") {
         return;
       }
       const sdp = JSON.parse(data.sdp);
