ec633af7a578a78823d1a2cd9f484402c39ae539
Jonathan Silverman <jsilverman2@gmail.com>
2021-05-20 19:42:41 -0600

    catch ding errors

--- src/assets/javascripts/channels/canvas_channel.js
+++ src/assets/javascripts/channels/canvas_channel.js
@@ -107,13 +107,12 @@ window.canvas_socket.connect()
                 }, 6000);
                 if (!window.nicks.includes(data["name"])) {
                     window.nicks.push(data["name"]);
-                    try {
+
                         var ding = new Audio("/ding.wav")
                         ding.volume = 0.25;
-                        ding.play()
-                    } catch {
-                        console.log("failed to online ding")
-                    }
+                        ding.play().catch((e) => {
+                        console.log(e.message)
+                        })
                 }
                 if ($(".online-" + data['name']).length == 0) {
                     $("#connected_users").html($("#connected_users").html() + `<li class='online-${data['name']}'>${data['name']}</li>`)
