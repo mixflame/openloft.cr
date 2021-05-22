7c7530d2c61f20cafdb13bf1e63ccdb505a139d0
Jonathan Silverman <jsilverman2@gmail.com>
2021-05-21 18:08:02 -0600

    try to block another double call

--- src/assets/javascripts/livepixel.js
+++ src/assets/javascripts/livepixel.js
@@ -599,7 +599,7 @@ function setMediaBitrates(sdp) {
       const offerCollision = (data.sdp.type == "offer") &&
                                (window.makingOffer || pc.signalingState != "stable");
       ignoreOffer = !polite && offerCollision;
-      if (ignoreOffer) {
+      if (ignoreOffer || isNegotiating) {
         return;
       }
       const sdp = JSON.parse(data.sdp);
