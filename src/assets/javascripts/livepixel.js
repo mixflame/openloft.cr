2d46280c1dcffd234dbcf534f2513c3547e11b1a
Jonathan Silverman <jsilverman2@gmail.com>
2021-05-23 13:33:32 -0600

    fix

--- src/assets/javascripts/livepixel.js
+++ src/assets/javascripts/livepixel.js
@@ -764,8 +764,8 @@ function setMediaBitrates(sdp) {
   var mouseDown = function(mousedown_name, color, size) {
     if(mousedown_name.match(/^[a-z0-9]+$/i)) {
       $("#status").html("<font class='who-drew' color='" + color + "'>" + mousedown_name + "</font> is drawing");
-      $(".online-" + mousedown_name).remove();
-      $("#connected_users").html($("#connected_users").html() + '<li class="online-'+ mousedown_name +'" style="color:' + color + ';">' + mousedown_name + '</div>')
+      // $(".online-" + mousedown_name).remove();
+      $(".online-" + mousedown_name).replaceWith('<li class="online-'+ mousedown_name +'" style="color:' + color + ';">' + mousedown_name + '</div>')
     }
   }
   window.mouseDown = mouseDown;
