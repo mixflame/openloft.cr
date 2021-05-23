ddbe31b81afdd5d0ac7b6f5b5ece01e8ac838ca3
Jonathan Silverman <jsilverman2@gmail.com>
2021-05-23 14:36:41 -0600

    disable whos online whos drawing

--- src/assets/javascripts/livepixel.js
+++ src/assets/javascripts/livepixel.js
@@ -765,7 +765,7 @@ function setMediaBitrates(sdp) {
     if(mousedown_name.match(/^[a-z0-9]+$/i)) {
       $("#status").html("<font class='who-drew' color='" + color + "'>" + mousedown_name + "</font> is drawing");
       // $(".online-" + mousedown_name).remove();
-      $(".online-" + mousedown_name).replaceWith('<li class="online-'+ mousedown_name +'" style="color:' + color + ';">' + mousedown_name + '</li>')
+      // $(".online-" + mousedown_name).replaceWith('<li class="online-'+ mousedown_name +'" style="color:' + color + ';">' + mousedown_name + '</li>')
     }
   }
   window.mouseDown = mouseDown;
