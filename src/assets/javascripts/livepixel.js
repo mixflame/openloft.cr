799c26925f0b6e6e0586c4c2282fde28a03f5a51
Jonathan Silverman <jsilverman2@gmail.com>
2021-05-27 15:09:00 -0600

    og chrome

--- src/assets/javascripts/livepixel.js
+++ src/assets/javascripts/livepixel.js
@@ -1045,7 +1045,7 @@ function setMediaBitrates(sdp) {
 			              // dy = points[i][1] - mpClickY[mpClickY.length - 1];
                     d = dx * dx + dy * dy;
                     // console.log(d);
-                    if (d < 20000)
+                    if (d < 1000)
                     {
                       // console.log(mpClickColor[i]);
                       // context.strokeStyle = "rgba(" + hexToRgb(mpClickColor[i]).r + ", " + hexToRgb(mpClickColor[i]).g + ", " + hexToRgb(mpClickColor[i]).b + ", " + 0.1 * mpClickSize[i] + " )";
