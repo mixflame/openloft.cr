41e69b3d8ed00f07b33b730ecec338783254ed22
Jonathan Silverman <jsilverman2@gmail.com>
2021-05-27 20:13:14 -0600

    fix chrome... use own version... better

--- src/assets/javascripts/livepixel.js
+++ src/assets/javascripts/livepixel.js
@@ -1036,11 +1036,11 @@ function setMediaBitrates(sdp) {
                     context.stroke();
 
 
-                    dx = points[i][0] - points[mpClickCount[i]][0];
-			              dy = points[i][1] - points[mpClickCount[i]][1];
+                    // dx = points[i][0] - points[mpClickCount[i]][0];
+			              // dy = points[i][1] - points[mpClickCount[i]][1];
                     // console.log(mpClickX[mpClickX.length - 2]);
-                    // dx = mpClickX[i] - mpClickX[mpClickX.length - 1];
-                    // dy = mpClickY[i] - mpClickY[mpClickY.length - 1];
+                    dx = mpClickX[0] - mpClickX[i];
+                    dy = mpClickY[0] - mpClickY[i];
                     // dx = points[i][0] - mpClickX[mpClickX.length - 1];
 			              // dy = points[i][1] - mpClickY[mpClickY.length - 1];
                     d = dx * dx + dy * dy;
