169c5f96c95f87cf66e317097dcd0c27715d71ef
Jonathan Silverman <jsilverman2@gmail.com>
2021-06-02 01:26:52 -0600

    random backgrounds

--- src/assets/javascripts/livepixel.js
+++ src/assets/javascripts/livepixel.js
@@ -2619,8 +2619,9 @@ function setMediaBitrates(sdp) {
       })
   }
 
+  // test
   const bgs = ["WallpaperDog-11333.jpg", "WallpaperDog-5525597.jpg", "WallpaperDog-5525602.jpg", "WallpaperDog-5525605.jpg", "WallpaperDog-5525688.jpg", "WallpaperDog-10851523.jpg", "WallpaperDog-10877066.jpg", "WallpaperDog-10877076.jpg"]
-  $("body").css("background-image", "url(/dist/images/" + bgs[Math.floor(Math.random() * bgs.length)] + ")")
+  $("body").css("background-image", "url(/" + bgs[Math.floor(Math.random() * bgs.length)] + ")")
 
   }
   
