d0c9d4ffbf08a129a8ed022289dd595e6d65da67
Jonathan Silverman <jsilverman2@gmail.com>
2021-06-02 21:16:04 -0600

    keep caret position

--- src/assets/javascripts/channels/text_channel.js
+++ src/assets/javascripts/channels/text_channel.js
@@ -3,6 +3,75 @@ import Amber from 'amber';
 var urlParams = new URLSearchParams(window.location.search);
 var room = urlParams.get('room');
 
+
+window.getInputSelection = function(el) {
+    var start = 0, end = 0, normalizedValue, range,
+        textInputRange, len, endRange;
+
+    if (typeof el.selectionStart == "number" && typeof el.selectionEnd == "number") {
+        start = el.selectionStart;
+        end = el.selectionEnd;
+    } else {
+        range = document.selection.createRange();
+
+        if (range && range.parentElement() == el) {
+            len = el.value.length;
+            normalizedValue = el.value.replace(/\r\n/g, "\n");
+
+            // Create a working TextRange that lives only in the input
+            textInputRange = el.createTextRange();
+            textInputRange.moveToBookmark(range.getBookmark());
+
+            // Check if the start and end of the selection are at the very end
+            // of the input, since moveStart/moveEnd doesn't return what we want
+            // in those cases
+            endRange = el.createTextRange();
+            endRange.collapse(false);
+
+            if (textInputRange.compareEndPoints("StartToEnd", endRange) > -1) {
+                start = end = len;
+            } else {
+                start = -textInputRange.moveStart("character", -len);
+                start += normalizedValue.slice(0, start).split("\n").length - 1;
+
+                if (textInputRange.compareEndPoints("EndToEnd", endRange) > -1) {
+                    end = len;
+                } else {
+                    end = -textInputRange.moveEnd("character", -len);
+                    end += normalizedValue.slice(0, end).split("\n").length - 1;
+                }
+            }
+        }
+    }
+
+    return {
+        start: start,
+        end: end
+    };
+}
+
+window.offsetToRangeCharacterMove = function(el, offset) {
+    return offset - (el.value.slice(0, offset).split("\r\n").length - 1);
+}
+
+window.setInputSelection = function(el, startOffset, endOffset) {
+    if (typeof el.selectionStart == "number" && typeof el.selectionEnd == "number") {
+        el.selectionStart = startOffset;
+        el.selectionEnd = endOffset;
+    } else {
+        var range = el.createTextRange();
+        var startCharMove = offsetToRangeCharacterMove(el, startOffset);
+        range.collapse(true);
+        if (startOffset == endOffset) {
+            range.move("character", startCharMove);
+        } else {
+            range.moveEnd("character", offsetToRangeCharacterMove(el, endOffset));
+            range.moveStart("character", startCharMove);
+        }
+        range.select();
+    }
+}
+
 window.setupText = () => {
     console.log("connected to /text")
     if(window.text_socket.channels.length == 0){
@@ -31,11 +100,10 @@ window.setupText = () => {
         console.log(newDoc)
         console.log(patch)
 
-        var cursorPosition = $("#collaborative_text").prop("selectionStart");
+        var sel = getInputSelection($("#collaborative_text")[0]);
         $("#collaborative_text").val(newDoc.text);
-        // $("#collaborative_text").focus()
-        $("#collaborative_text").prop("selectionStart", cursorPosition - 1);
-        $("#collaborative_text").prop("selectionEnd", cursorPosition - 1);
+        $("#collaborative_text").focus()
+        setInputSelection($("#collaborative_text")[0], sel.start, sel.end);
 
         currentDoc = newDoc;
     })
