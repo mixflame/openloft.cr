e7e333f4959b4ccbc9445b7e97952ea87bc10c67
Jonathan Silverman <jsilverman2@gmail.com>
2021-06-18 04:32:09 -0600

    give devices 60 seconds to load

--- src/assets/javascripts/channels/chat_channel.js
+++ src/assets/javascripts/channels/chat_channel.js
@@ -81,7 +81,7 @@ window.setupChat = () => {
                 const n = window.nicks[nick];
                 window.last_ping[n] = Date.now();
                 setInterval(() => {
-                    if (window.last_ping[n] < Date.now() - (10000)) {
+                    if (window.last_ping[n] < Date.now() - (60000)) {
                         $(".online-" + n).remove()
                         arrayRemove(window.nicks, n);
                     }
