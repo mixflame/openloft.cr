3164894c16c1ab818f33393d146fd10eb71c8165
Jonathan Silverman <jsilverman2@gmail.com>
2021-06-18 02:29:27 -0600

    shorter pingout

--- src/assets/javascripts/channels/chat_channel.js
+++ src/assets/javascripts/channels/chat_channel.js
@@ -6,13 +6,13 @@ var room = urlParams.get('room');
 
 window.setupChat = () => {
     console.log("connected to /chat")
-    if(window.chat_socket.channels.length == 0){
+    if (window.chat_socket.channels.length == 0) {
         window.chat_channel = chat_socket.channel('chat:' + window.room)
         window.chat_channel.join()
     }
     if (!window.dontLog) console.log("Connected to chat channel");
 
-    window.chat_channel.push("message_new", {online: true, name: window.name});
+    window.chat_channel.push("message_new", { online: true, name: window.name });
 
     window.start_pinging();
     setInterval(window.start_pinging, 5000);
@@ -23,14 +23,14 @@ window.setupChat = () => {
         if (data["ping"]) {
             window.last_ping[data['name']] = Date.now();
             setTimeout(() => {
-                if (window.last_ping[data['name']] < Date.now() - (1000 * 60 * 4)) {
+                if (window.last_ping[data['name']] < Date.now() - (1000 * 10)) {
                     $(".online-" + data['name']).remove()
                     var dong = new Audio("/dong.wav")
                     dong.volume = 0.5;
                     dong.play().catch((e) => {
-                    console.log(e.message)
+                        console.log(e.message)
                     })
-                }    
+                }
             }, 6000);
             return;
         }
@@ -44,7 +44,7 @@ window.setupChat = () => {
         // Called when there's incoming data on the websocket for this channel
         $("#chat_area").html($("#chat_area").html() + "<p style='margin: 0px;'>" + "&lt" + data["name"] + "&gt" + data["chat_message"] + "</p>");
 
-        $("img").one("load", function () {
+        $("img").one("load", function() {
             window.scroll_to_bottom();
         });
 
@@ -61,23 +61,23 @@ window.setupChat = () => {
         console.log(data);
 
 
-        
 
-        if(data["name"] != window.name) {
-            if(window.nicks.length < data["nicks"].length) {
+
+        if (data["name"] != window.name) {
+            if (window.nicks.length < data["nicks"].length) {
                 //someone came online
                 var ding = new Audio("/ding.wav")
                 ding.volume = 0.5;
                 ding.play().catch((e) => {
-                console.log(e.message)
+                    console.log(e.message)
                 })
-            } else if(window.nicks.length > data["nicks"].length) {
+            } else if (window.nicks.length > data["nicks"].length) {
                 // someone went away
                 var dong = new Audio("/dong.wav")
                 dong.volume = 0.5;
                 dong.play().catch((e) => {
-                console.log(e.message)
-                })    
+                    console.log(e.message)
+                })
             }
         }
 
@@ -89,26 +89,26 @@ window.setupChat = () => {
                 const n = window.nicks[nick];
                 window.last_ping[n] = Date.now();
                 setTimeout(() => {
-                    if (window.last_ping[n] < Date.now() - (1000 * 60 * 4)) {
+                    if (window.last_ping[n] < Date.now() - (1000 * 10)) {
                         $(".online-" + n).remove()
                         var dong = new Audio("/dong.wav")
                         dong.volume = 0.5;
                         dong.play().catch((e) => {
-                        console.log(e.message)
+                            console.log(e.message)
                         })
-                    }    
+                    }
                 }, 6000);
                 $("#connected_users").html($("#connected_users").html() + `<li class='online-${n}'>${n}</li>`)
             }
         }
-        
+
 
         var parent = $("#connected_users")[0],
             // take items (parent.children) into array
             itemsArray = Array.prototype.slice.call(parent.children);
 
         // sort items in array by custom criteria
-        itemsArray.sort(function (a, b) {
+        itemsArray.sort(function(a, b) {
             // inner text suits best (even when formated somehow)
             if (a.innerText < b.innerText) return -1;
             if (a.innerText > b.innerText) return 1;
@@ -116,7 +116,7 @@ window.setupChat = () => {
         });
 
         // reorder items in the DOM
-        itemsArray.forEach(function (item) {
+        itemsArray.forEach(function(item) {
             // one by one move to the end in correct order
             parent.appendChild(item);
         });
@@ -128,11 +128,11 @@ window.setupChat = () => {
 window.chat_socket = new Amber.Socket('/chat')
 chat_socket.connect()
     .then(setupChat)
-    window.chat_socket._reconnect = () => {
-        clearTimeout(window.chat_socket.reconnectTimeout)
-        window.chat_socket.reconnectTimeout = setTimeout(() => {
-          window.chat_socket.reconnectTries++
-          window.chat_socket.connect(window.chat_socket.params).then(setupChat);
-          window.chat_socket._reconnect()
-        }, window.chat_socket._reconnectInterval())
-      }
+window.chat_socket._reconnect = () => {
+    clearTimeout(window.chat_socket.reconnectTimeout)
+    window.chat_socket.reconnectTimeout = setTimeout(() => {
+        window.chat_socket.reconnectTries++
+            window.chat_socket.connect(window.chat_socket.params).then(setupChat);
+        window.chat_socket._reconnect()
+    }, window.chat_socket._reconnectInterval())
+}
